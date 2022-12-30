66ba57d44842dfbced0325d95d57ba7aabcf4be6
Kathy Nguyen <kathyn262@gmail.com>
2022-12-29 17:31:21 -0800

    added char data

--- backend/routes/characters.js
+++ backend/routes/characters.js
@@ -1,21 +1,42 @@
 const express = require("express");
 const router = new express.Router();
-const axios = require('axios');
-// const Villager = require("../models/villager");
+const axios = require("axios");
 
-/** 
+/**
  * route for searching by villager name
  * GET ?name=villager  =>  {villager: villager} */
 
 router.get("/", async function (req, res, next) {
   try {
-    const villager = await axios.get("https://api.genshin.dev/characters/amber");
-    let test = res.json(villager.data);
-    return test;
+    const allCharCall = await axios.get(
+      "https://api.genshin.dev/characters/all"
+    );
+    let allCharData = allCharCall.data;
+
+    const allCharIdCall = await axios.get(
+      "https://api.genshin.dev/characters"
+    );
+    let allCharIds = allCharIdCall.data;
+
+    let reformattedCharData = [];
+    // works bc lengths of allCharData and allCharIds are the same
+    let i = 0;
+
+    // todo: add image
+    for (char of allCharData) {
+      let currChar = {};
+      currChar['name'] = char['name'];
+      currChar['id'] = allCharIds[i];
+      currChar['vision'] = char['vision'];
+      reformattedCharData.push(currChar);
+      i++;
+    }
+
+
+    return res.json(reformattedCharData);
   } catch (err) {
     return next(err);
   }
 });
-
 
 module.exports = router;
