0709a855f09856e9d71c6af6b8adaaade5977ead
Kathy Nguyen <kathyn262@gmail.com>
2023-01-10 15:17:17 -0800

    added comments

--- backend/routes/characters.js
+++ backend/routes/characters.js
@@ -3,20 +3,23 @@ const router = new express.Router();
 const axios = require("axios");
 
 /**
- * route for searching by villager name
- * GET ?name=villager  =>  {villager: villager} */
+ * route for searching by character name
+ * GET ?name=character  =>  {character: character} */
 
 router.get("/", async function (req, res, next) {
   try {
+    // character id and data are stored separately on api
+    
+    const allCharIdCall = await axios.get(
+      "https://api.genshin.dev/characters"
+    );
+    let allCharIds = allCharIdCall.data;
+
     const allCharCall = await axios.get(
       "https://api.genshin.dev/characters/all"
     );
     let allCharData = allCharCall.data;
 
-    const allCharIdCall = await axios.get(
-      "https://api.genshin.dev/characters"
-    );
-    let allCharIds = allCharIdCall.data;
 
     let reformattedCharData = [];
     // works bc lengths of allCharData and allCharIds are the same
