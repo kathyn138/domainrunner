f0d1319bfea4c2e9cf9f78f541820fc373658d21
Kathy Nguyen <kathyn262@gmail.com>
2023-01-10 20:55:07 -0800

    added data

---
+++ backend/data/elements.js
@@ -0,0 +1,37 @@
+module.exports = {
+  geo: {
+    id: "geo",
+    name: "Geo",
+    simpleName: "Stone",
+  },
+  anemo: {
+    id: "anemo",
+    name: "Anemo",
+    simpleName: "Wind",
+  },
+  pyro: {
+    id: "pyro",
+    name: "Pyro",
+    simpleName: "Fire",
+  },
+  hydro: {
+    id: "hydro",
+    name: "Hydro",
+    simpleName: "Water",
+  },
+  electro: {
+    id: "electro",
+    name: "Electro",
+    simpleName: "Electro",
+  },
+  dendro: {
+    id: "dendro",
+    name: "Dendro",
+    simpleName: "Grass",
+  },
+  cryo: {
+    id: "cryo",
+    name: "Cryo",
+    simpleName: "Ice",
+  },
+};
