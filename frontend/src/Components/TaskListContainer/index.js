b6ffc190f98d5754cfe0a46bc2ee9b894020ae1a
Kathy Nguyen <kathyn262@gmail.com>
2023-01-30 19:52:11 -0800

    added component skeleton

--- frontend/src/Components/TaskListContainer/index.js
+++ frontend/src/Components/TaskListContainer/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function TaskListContainer() {
-  return 'task list container';
+  console.log('here in task list container')
+  return (
+    <div className="App">
+      <header className="App-header">
+        tasklistcontainer
+      </header>
+    </div>
+  );
 }
 
 export default TaskListContainer;
