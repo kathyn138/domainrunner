9abc62e6f65dc71f0001db2841e1ef6f4400d9c0
Kathy Nguyen <kathyn262@gmail.com>
2023-01-30 19:52:18 -0800

    added routes

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import RouteList from './RouteList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         hi
+        <RouteList />
       </header>
     </div>
   );
--- frontend/src/Components/RouteList.js
+++ frontend/src/Components/RouteList.js
@@ -1,20 +1,18 @@
-import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
-import Home from "./Home";
-import EntityContainer from "./EntityContainer";
-import TaskListContainer from "./TaskListContainer";
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Home';
+import EntityContainer from './EntityContainer';
+import TaskListContainer from './TaskListContainer';
 
 function RouteList() {
   return (
-    <React.Fragment>
-      <Routes>
-        <Route exact path="/" render={() => <Home />} />
-        <Route exact path="/characters" render={() => <EntityContainer />} />
-        <Route exact path="/weapons" render={() => <EntityContainer />} />
-        <Route exact path="/tasklist" render={() => <TaskListContainer />} />
-      </Routes>
-      {/* <Navigate to="/" /> */}
-    </React.Fragment>
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/characters" element={<EntityContainer />} />
+      <Route exact path="/weapons" element={<EntityContainer />} />
+      <Route exact path="/tasklist" element={<TaskListContainer />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
 
