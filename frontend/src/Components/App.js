a749330426a0bb945bd14336200535832ecf342b
Kathy Nguyen <kathyn262@gmail.com>
2023-02-22 16:55:29 -0800

    set up navbar skeleton

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -4,7 +4,28 @@ import RouteList from './RouteList';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container-fluid">
+      <div className="container">
+        <nav class="navbar fixed-top bg-body-tertiary">
+          <div class="container">
+            <a class="navbar-brand" href="#">
+              Home
+            </a>
+            <a class="navbar-brand" href="#">
+              Characters
+            </a>
+            <a class="navbar-brand" href="#">
+              Weapons
+            </a>
+            <a class="navbar-brand" href="#">
+              Calendar
+            </a>
+            <a class="navbar-brand" href="#">
+              Cart
+            </a>
+          </div>
+        </nav>
+      </div>
       <header className="App-header">
         hi
         <RouteList />
