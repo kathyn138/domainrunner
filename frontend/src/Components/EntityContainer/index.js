39662673265b5bd4e8fec573776aa90d7cb41403
Kathy Nguyen <kathyn262@gmail.com>
2023-02-22 16:18:07 -0800

    set up data on mount

--- frontend/src/Components/EntityContainer/index.js
+++ frontend/src/Components/EntityContainer/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import axios from 'axios';
 
-function EntityContainer() {
-  console.log('e');
-  return 'entity container';
+function EntityContainer(props) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [entityList, setEntityList] = useState([]);
+
+  useEffect(function fetchEntityWhenMounted() {
+    async function fetchEntity() {
+      const userResult = await axios.get(`http://localhost:5000/${props.entity}`);
+      setEntityList(userResult.data);
+      setIsLoading(false);
+    }
+    fetchEntity();
+  }, []);
+
+  if (isLoading) return <i>Loading...</i>;
+
+  return (
+    console.log(entityList)
+  )
 }
 
 export default EntityContainer;
