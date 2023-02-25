fe5652152b7e42963f0c6ea745958186b0d3d703
Kathy Nguyen <kathyn262@gmail.com>
2023-02-24 17:19:28 -0800

    moved navbar

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,31 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
 import RouteList from './RouteList';
 
 function App() {
   return (
     <div className="container-fluid">
-      <div className="container">
-        <nav class="navbar fixed-top bg-body-tertiary">
-          <div class="container">
-            <a class="navbar-brand" href="#">
-              Home
-            </a>
-            <a class="navbar-brand" href="#">
-              Characters
-            </a>
-            <a class="navbar-brand" href="#">
-              Weapons
-            </a>
-            <a class="navbar-brand" href="#">
-              Calendar
-            </a>
-            <a class="navbar-brand" href="#">
-              Cart
-            </a>
-          </div>
-        </nav>
-      </div>
+      <NavBar />
       <header className="App-header">
         hi
         <RouteList />
--- frontend/src/Components/NavBar/index.js
+++ frontend/src/Components/NavBar/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 function NavBar() {
-  return 'navbar';
+  return (
+    <div className="container">
+      <nav class="navbar fixed-top bg-body-tertiary">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            Home
+          </a>
+          <a class="navbar-brand" href="#">
+            Characters
+          </a>
+          <a class="navbar-brand" href="#">
+            Weapons
+          </a>
+          <a class="navbar-brand" href="#">
+            Calendar
+          </a>
+          <a class="navbar-brand" href="#">
+            Cart
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
