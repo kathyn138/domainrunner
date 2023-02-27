88d1f2528cbd51f66869407e096bb37780f9ec7e
Kathy Nguyen <kathyn262@gmail.com>
2023-02-27 15:37:05 -0800

    added classname

--- frontend/src/Components/NavBar/index.js
+++ frontend/src/Components/NavBar/index.js
@@ -3,22 +3,22 @@ import './NavBar.css';
 
 function NavBar() {
   return (
-      <nav class="navbar fixed-top bg-body-tertiary">
+      <nav className="navbar fixed-top bg-body-tertiary">
       <span className="navbar-icon-span"><img className="navbar-icon" src="./favicon.ico" alt="favicon"></img></span>
-        <div class="container navbar-container">
-          <a class="navbar-brand" href="#">
+        <div className="container navbar-container">
+          <a className="navbar-brand" href="#">
             Home
           </a>
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Characters
           </a>
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Weapons
           </a>
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Calendar
           </a>
-          <a class="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             Cart
           </a>
         </div>
