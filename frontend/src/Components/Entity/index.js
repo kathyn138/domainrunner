bf7e918130bfe2887b5b81a99b8902eeee7b31c0
Kathy Nguyen <kathyn262@gmail.com>
2023-02-27 18:44:34 -0800

    chose horizontal heart style

--- frontend/src/Components/Entity/index.js
+++ frontend/src/Components/Entity/index.js
@@ -7,33 +7,21 @@ function Entity() {
       <img
         className="card-img-top"
         src="https://paimon.moe/images/characters/albedo.png"
-        alt="Card image cap">
-      </img>
+        alt="Card image cap"
+      ></img>
       <div className="huh"></div>
       <div className="card-body">
         <h8 className="card-title">
           <b>Albedo</b>
         </h8>
-        <span className="element-icon">
-          <img
-            src="https://paimon.moe/images/elements/pyro.png"
-            alt="element"
-          ></img>
-        </span>
         <p className="card-text">
-        <i className="fas fa-heart filled-in-heart"></i>
-        </p>
 
-        {/* for element icon next to heart */}
-        {/* <p className="card-text">
-          <span className="element-icon">
-            <img
+            <img className="element-icon"
               src="https://paimon.moe/images/elements/pyro.png"
               alt="element"
             ></img>
-          </span>
           <i className="fas fa-heart filled-in-heart"></i>
-        </p> */}
+        </p>
       </div>
     </div>
   );
