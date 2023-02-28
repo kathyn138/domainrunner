14fff7a831a7bee7143801c328d2cdc253889417
Kathy Nguyen <kathyn262@gmail.com>
2023-02-27 18:39:35 -0800

    added two heart styles

--- frontend/src/Components/Entity/index.js
+++ frontend/src/Components/Entity/index.js
@@ -4,14 +4,36 @@ import './Entity.css';
 function Entity() {
   return (
     <div className="card align-items-center entity-card">
-      <img className="card-img-top" src="https://paimon.moe/images/characters/albedo.png" alt="Card image cap">
+      <img
+        className="card-img-top"
+        src="https://paimon.moe/images/characters/albedo.png"
+        alt="Card image cap">
       </img>
       <div className="huh"></div>
       <div className="card-body">
-        <h8 className="card-title"><b>AlbedoAlbedoAlbedoAlbedoAlbedoAlbedoAlbedoAlbedo</b><span className="entity-heart">hi</span></h8>
+        <h8 className="card-title">
+          <b>Albedo</b>
+        </h8>
+        <span className="element-icon">
+          <img
+            src="https://paimon.moe/images/elements/pyro.png"
+            alt="element"
+          ></img>
+        </span>
         <p className="card-text">
-         {`<3`}
+        <i className="fas fa-heart filled-in-heart"></i>
         </p>
+
+        {/* for element icon next to heart */}
+        {/* <p className="card-text">
+          <span className="element-icon">
+            <img
+              src="https://paimon.moe/images/elements/pyro.png"
+              alt="element"
+            ></img>
+          </span>
+          <i className="fas fa-heart filled-in-heart"></i>
+        </p> */}
       </div>
     </div>
   );
