608e042df3f53394ee895a670cb01dde520e9383
Kathy Nguyen <kathyn262@gmail.com>
2023-02-27 17:21:35 -0800

    renamed entity container

--- frontend/src/Components/RouteList.js
+++ frontend/src/Components/RouteList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Home';
-import EntityContainer from './EntityContainer';
+import EntityList from './EntityList';
 import TaskListContainer from './TaskListContainer';
 
 function RouteList() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/characters" element={<EntityContainer entity="characters"/>} />
-      <Route exact path="/weapons" element={<EntityContainer entity="weapons"/>} />
+      <Route exact path="/characters" element={<EntityList entity="characters"/>} />
+      <Route exact path="/weapons" element={<EntityList entity="weapons"/>} />
       <Route exact path="/tasklist" element={<TaskListContainer />} />
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
