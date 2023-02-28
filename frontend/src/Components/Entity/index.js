0376011532afaa3454b53fde65fe4b7ffce329cd
Kathy Nguyen <kathyn262@gmail.com>
2023-02-27 19:32:48 -0800

    added grid style cards

--- frontend/src/Components/Entity/index.js
+++ frontend/src/Components/Entity/index.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import './Entity.css';
 
-function Entity() {
+function Entity(props) {
   return (
+    <div className="col">
+
     <div className="card align-items-center entity-card">
+      {/* TODO:  add color/bg to char icon depending on their element
+       * issue lies with if entity list represents weapons
+       */}
+
+       {/* TODO: fix multi line names
+       * makes the cards look uneven rn
+       */}
       <img
         className="card-img-top"
-        src="https://paimon.moe/images/characters/albedo.png"
+        src={`https://paimon.moe/images/characters/${props.id}.png`}
         alt="Card image cap"
       ></img>
-      <div className="huh"></div>
       <div className="card-body">
-        <h8 className="card-title">
-          <b>Albedo</b>
-        </h8>
+        <h6 className="card-title">
+          <b>{props.name}</b>
+        </h6>
         <p className="card-text">
-
-            <img className="element-icon"
-              src="https://paimon.moe/images/elements/pyro.png"
-              alt="element"
-            ></img>
-          <i className="fas fa-heart filled-in-heart"></i>
+          <img
+            className="element-icon"
+            src={`https://paimon.moe/images/elements/${props.element}.png`}
+            alt="element"
+          ></img>
+          <span className="heart-icon">
+            <i className="fas fa-heart filled-in-heart"></i>
+          </span>
         </p>
       </div>
+    </div>
     </div>
   );
 }
