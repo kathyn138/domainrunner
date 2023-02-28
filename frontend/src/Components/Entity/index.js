3dba4fa5f4f50d46270f46b51e9e46f4f48ec187
Kathy Nguyen <kathyn262@gmail.com>
2023-02-28 15:23:10 -0800

    removed finished todo

--- frontend/src/Components/Entity/index.js
+++ frontend/src/Components/Entity/index.js
@@ -11,35 +11,30 @@ function Entity(props) {
 
   return (
     <div className="col">
-
-    <div className="card align-items-center entity-card">
-      {/* TODO:  add color/bg to char icon depending on their element
-       * issue lies with if entity list represents weapons
-       */}
-      <div className={`card-top-${props.element}`}>
-
-      <img
-        className="card-top-img"
-        src={`https://paimon.moe/images/characters/${props.id}.png`}
-        alt="Card image cap"
-      ></img>
-      </div>
-      <div className="card-body">
-        <h6 className={`card-title ${nameType}`}>
-          <b>{props.name}</b>
-        </h6>
-        <p className="card-text">
+      <div className="card align-items-center entity-card">
+        <div className={`card-top-${props.element}`}>
           <img
-            className="element-icon"
-            src={`https://paimon.moe/images/elements/${props.element}.png`}
-            alt="element"
+            className="card-top-img"
+            src={`https://paimon.moe/images/characters/${props.id}.png`}
+            alt="Card image cap"
           ></img>
-          <span className="heart-icon">
-            <i className="fas fa-heart filled-in-heart"></i>
-          </span>
-        </p>
+        </div>
+        <div className="card-body">
+          <h6 className={`card-title ${nameType}`}>
+            <b>{props.name}</b>
+          </h6>
+          <p className="card-text">
+            <img
+              className="element-icon"
+              src={`https://paimon.moe/images/elements/${props.element}.png`}
+              alt="element"
+            ></img>
+            <span className="heart-icon">
+              <i className="fas fa-heart filled-in-heart"></i>
+            </span>
+          </p>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
