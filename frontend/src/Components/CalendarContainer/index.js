df2aaff20e77be6d75409559a13fc48ded842bd2
Kathy Nguyen <kathyn262@gmail.com>
2023-03-09 15:22:57 -0800

    added calendar api call

--- frontend/src/Components/CalendarContainer/index.js
+++ frontend/src/Components/CalendarContainer/index.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
+const BASE_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:5000';
 
 function CalendarContainer() {
+  const [calendar, setCalendar] = useState([]);
+  const cartStoreData = useSelector((store) => store.cart);
+
+  useEffect(
+    function generateCalendarWhenMounted() {
+      async function generateCalendar() {
+        if (cartStoreData) {
+          let calendarResponse = await axios.post(
+            `${BASE_URL}/calendar`,
+            cartStoreData
+          );
+          setCalendar(calendarResponse.data);
+        }
+      }
+      generateCalendar();
+    },
+    [cartStoreData]
+  );
+
+  console.log(calendar);
   return (
-    <div className="App">
-      <header className="App-header">
-        CalendarContainer
-      </header>
+    <div className="col-8 text-center">
+      <div className="row">
+        <h3 className="something">
+          <b>calenar title</b>
+        </h3>
+      </div>
+      <div className="row">calendar row 2</div>
     </div>
   );
 }
