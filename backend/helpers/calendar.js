deba1bb6c3c6d06d3a0069128a0d9f4c5c673421
Kathy Nguyen <kathyn262@gmail.com>
2023-03-20 20:00:16 -0700

    moved cal logic to helper

---
+++ backend/helpers/calendar.js
@@ -0,0 +1,75 @@
+const formatItemData = require('./helpers/formatData');
+
+/**
+ * structure of ascension data:
+ * array of obj, where each obj represents
+ * an ascension level and its required items
+ * [
+ *  {
+ *    items: [{items: [{item, ...}, ...], ...}]
+ *  },
+ *  {...}
+ * ]
+ */
+function getAscensionItems(entity, ascension) {
+  let items = {};
+
+  for (const level in ascension) {
+    for (const itemList in ascension[level]) {
+      for (const i in ascension[level][itemList]) {
+        let currRawItem = ascension[level][itemList][i].item;
+
+        /**
+         * some items repeat across ascension levels.
+         * want to generate unique entity item pairings
+         */
+        if (!items[currRawItem.id]) {
+          let formattedItem = formatItemData(currRawItem);
+
+          formattedItem['entityId'] = entity.id;
+          formattedItem['entityIcon'] = entity.icon;
+          formattedItem['entityType'] = entity.type;
+
+          items[currRawItem.id] = formattedItem;
+        }
+      }
+    }
+  }
+
+  return items;
+}
+
+// sort items in each day alphabetically
+function alphabetizeItems(cal) {
+  for (const day in cal) {
+    /**
+     * can't a.itemId - b.itemId bc need to convert str to num
+     * either use ternary or localeCompare()
+     */
+    cal[day].sort((a, b) =>
+      a.itemId < b.itemId ? -1 : a.itemId > b.itemId ? 1 : 0
+    );
+  }
+}
+
+/**
+ * organize items by days
+ * then alphabetize items
+ */
+function addToCalendar(items, cal) {
+  for (let currItem in items) {
+    let availableDays = items[currItem].day;
+
+    if (availableDays === 'any') {
+      cal['any'].push(items[currItem]);
+    } else {
+      for (const day of availableDays) {
+        cal[day].push(items[currItem]);
+      }
+    }
+  }
+
+  alphabetizeItems(cal);
+}
+
+module.exports = { getAscensionItems, alphabetizeItems, addToCalendar };
--- backend/routes/calendar.js
+++ backend/routes/calendar.js
@@ -2,22 +2,26 @@ const express = require('express');
 const router = new express.Router();
 const rawCharData = require('../data/characters');
 const rawWeaponData = require('../data/weaponList');
+const addToCalendar = require('../helpers/calendar');
+const getAscensionItems = require('../helpers/calendar');
 
 /**
  * route for generating calendar
  * POST /  =>
- * {monday: [{itemId: '',
- * itemId: '',
- * itemIcon: '',
- * day: '',
- * entityId: '',
- * entityIcon: '',
- * entityType: ''}, ...],
- * tuesday: [...],
- * ...
- * any: [...]} 
+ * {
+ *  monday:
+ *   [{itemId: '',
+ *   itemId: '',
+ *   itemIcon: '',
+ *   day: '',
+ *   entityId: '',
+ *   entityIcon: '',
+ *   entityType: ''}, ...],
+ *  tuesday: [...],
+ *  ...,
+ *  any: [...]
+ * }
  * */
-
 router.post('/', async function (req, res, next) {
   try {
     let calendar = {
@@ -32,99 +36,11 @@ router.post('/', async function (req, res, next) {
     };
 
     /**
-     * rawItem data has excess info
-     * desired data structure for each formatted item:
-     * { itemId: '...', itemIcon: '...', day: [...] }
+     * cart has up to two categories:
+     * 'characters' and 'weapons'.
+     * process each category one at a time
+     * and add to calendar. 
      */
-    function formatItemData(rawItem) {
-      const { id, day } = rawItem;
-      let formattedItem = {};
-
-      formattedItem['itemId'] = id;
-      formattedItem['itemIcon'] = `https://paimon.moe/images/items/${id}.png`;
-
-      // not all items have limited availability
-      if (!day) {
-        formattedItem['day'] = 'any';
-      } else {
-        formattedItem['day'] = [...day];
-      }
-
-      return formattedItem;
-    }
-
-    /**
-     * structure of ascension:
-     * array of obj, where each obj represents
-     * an ascension level and its required items
-     * [
-     *  {
-     *    items: [{items: [{item, ...}, ...], ...}]
-     *  },
-     *  {...}
-     * ]
-     */
-    function getAscensionItems(entity, ascension) {
-      let items = {};
-
-      for (const level in ascension) {
-        for (const itemList in ascension[level]) {
-          for (const i in ascension[level][itemList]) {
-            let currRawItem = ascension[level][itemList][i].item;
-
-            /**
-             * some items repeat across ascension levels.
-             * want to generate unique entity item pairings
-             */
-            if (!items[currRawItem.id]) {
-              let formattedItem = formatItemData(currRawItem);
-
-              formattedItem['entityId'] = entity.id;
-              formattedItem['entityIcon'] = entity.icon;
-              formattedItem['entityType'] = entity.type;
-
-              items[currRawItem.id] = formattedItem;
-            }
-          }
-        }
-      }
-
-      return items;
-    }
-
-    // sort items in calendar alphabetically
-    function alphabetizeItems(cal) {
-      for (const day in cal) {
-        /**
-         * can't a.itemId - b.itemId bc need to convert str to num
-         * either use ternary or localeCompare()
-         */
-        cal[day].sort((a, b) =>
-          a.itemId < b.itemId ? -1 : a.itemId > b.itemId ? 1 : 0
-        );
-      }
-    }
-
-    /**
-     * organize entity item pairings by days
-     * then alphabetize items
-     */
-    function addToCalendar(charItems) {
-      for (let currItem in charItems) {
-        let availableDays = charItems[currItem].day;
-
-        if (availableDays === 'any') {
-          calendar['any'].push(charItems[currItem]);
-        } else {
-          for (const day of availableDays) {
-            calendar[day].push(charItems[currItem]);
-          }
-        }
-      }
-
-      alphabetizeItems(calendar);
-    }
-
     function processCart(cart) {
       if (cart.length) {
         for (const entity of cart) {
@@ -141,7 +57,7 @@ router.post('/', async function (req, res, next) {
             dataCategory[entity.id].ascension
           );
 
-          addToCalendar(currEntityItems);
+          addToCalendar(currEntityItems, calendar);
         }
       }
     }
