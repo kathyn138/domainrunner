a04a3ee521d99065ac805a3f0035cbc7d020f2ac
Kathy Nguyen <kathyn262@gmail.com>
2023-03-27 10:43:22 -0700

    removed reportwebvitals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import './index.css';
@@ -31,7 +30,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
