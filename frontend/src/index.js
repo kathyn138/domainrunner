71f7768770f1983e8b7f42df4baf0f6afd55b689
Kathy Nguyen <kathyn262@gmail.com>
2023-03-28 18:00:14 -0700

    refactored compose for apply bug

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,14 +10,12 @@ import rootReducer from './Reducers/root';
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ 
-    && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeEnhancers(applyMiddleware(thunk))
 );
 
 root.render(
