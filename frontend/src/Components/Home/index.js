830e64cf3efa993b0365e708a7286c8395f1c3a7
Kathy Nguyen <kathyn262@gmail.com>
2024-04-09 16:21:12 -0700

    updated asset srcs

--- frontend/src/Components/Home/index.js
+++ frontend/src/Components/Home/index.js
@@ -7,8 +7,8 @@ function Home() {
       <div className="row">
         <div className="col">
           <img
-            src="https://cdn.discordapp.com/attachments/709286174879121519/1085313667924496574/E2-VnK2X0AAKYxT.png?ex=65e78e9e&is=65d5199e&hm=3ae215c240525ceffc076a457ddf9a3775c32a12e29a997a9c980db9adc4f011&"
-            alt=""
+            src="../../public/paimon-home.png"
+            alt="home logo"
           ></img>
         </div>
         <div className="col">
